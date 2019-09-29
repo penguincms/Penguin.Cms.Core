6f4f8bd4cfa66d2cd030383c7c9569b38c26e7a1
MrJackSpade <loffredo.ap@gmail.com>
2019-09-29 09:18:10 -0700

    Add files via upload

---
+++ Interfaces/IGenerateDefaultEntities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Cms.Core.Interfaces
+{
+    public interface IGenerateDefaultEntities<T>
+    {
+        IEnumerable<T> Generate();
+    }
+}
